52efad1c1ff43fd4a235ef1b69aafd816139cada
Quake <QuakeEye@users.noreply.GitHub.com>
2024-12-13 00:56:23 +0100

    feat: finish day 12 part 2

--- 2024/12/day12-2.ts
+++ 2024/12/day12-2.ts
@@ -81,10 +81,82 @@ const getRegions = () => {
 
 
 // Calculate costs
+const boolToNumber : (input: boolean) => number = (input: boolean) => input ? 1 : 0;
 
 // Calculate corners as that equals the amount of sides
 const getCorners : (region: number[][], key: string) => number = (region: number[][], key: string) => {
-    
+    let cornerCount : number = 0;
+
+    region.forEach((cell) => {
+        // Determine outie corners
+        //
+        //      ...
+        //      .XX
+        //      .XX
+        const left = [cell[0] - 1, cell[1]];
+        const up = [cell[0], cell[1] - 1];
+        const right = [cell[0] + 1, cell[1]];
+        const down = [cell[0], cell[1] + 1];
+        
+        const topLeftOutie : boolean =      (outOfGrid(up[0], up[1]) || lineArr[up[1]][up[0]] !== key) && 
+                                            (outOfGrid(left[0], left[1]) || lineArr[left[1]][left[0]] !== key);
+        const topRightOutie : boolean =     (outOfGrid(up[0], up[1]) || lineArr[up[1]][up[0]] !== key) && 
+                                            (outOfGrid(right[0], right[1]) || lineArr[right[1]][right[0]] !== key);
+        const bottomLeftOutie : boolean =   (outOfGrid(down[0], down[1]) || lineArr[down[1]][down[0]] !== key) && 
+                                            (outOfGrid(left[0], left[1]) || lineArr[left[1]][left[0]] !== key);
+        const bottomRightOutie : boolean =  (outOfGrid(down[0], down[1]) || lineArr[down[1]][down[0]] !== key) && 
+                                            (outOfGrid(right[0], right[1]) || lineArr[right[1]][right[0]] !== key);
+
+        // Determine innie corners
+        //
+        //      .XX
+        //      XXX
+        //      XXX
+        const topLeft = [cell[0] - 1, cell[1] - 1];
+        const topRight = [cell[0] + 1, cell[1] - 1];
+        const bottomLeft = [cell[0] - 1, cell[1] + 1];
+        const bottomRight = [cell[0] + 1, cell[1] + 1];
+
+        let topLeftInnie : boolean = false;
+        let topRightInnie : boolean = false;
+        let bottomLeftInnie : boolean = false;
+        let bottomRightInnie : boolean = false;
+
+        if (!outOfGrid(up[0], up[1]) && !outOfGrid(left[0], left[1])) {
+            topLeftInnie =      (!outOfGrid(up[0], up[1]) && lineArr[up[1]][up[0]] === key) && 
+                                (!outOfGrid(left[0], left[1]) && lineArr[left[1]][left[0]] === key) && 
+                                (lineArr[topLeft[1]][topLeft[0]] !== key);
+        }
+
+        if (!outOfGrid(up[0], up[1]) && !outOfGrid(right[0], right[1])) {
+            topRightInnie =     (!outOfGrid(up[0], up[1]) && lineArr[up[1]][up[0]] === key) && 
+                                (!outOfGrid(right[0], right[1]) && lineArr[right[1]][right[0]] === key) && 
+                                (lineArr[topRight[1]][topRight[0]] !== key);
+        }
+
+        if (!outOfGrid(down[0], down[1]) && !outOfGrid(left[0], left[1])) {
+            bottomLeftInnie =   (!outOfGrid(down[0], down[1]) && lineArr[down[1]][down[0]] === key) && 
+                                (!outOfGrid(left[0], left[1]) && lineArr[left[1]][left[0]] === key) && 
+                                (lineArr[bottomLeft[1]][bottomLeft[0]] !== key);
+        }
+
+        if (!outOfGrid(down[0], down[1]) && !outOfGrid(right[0], right[1])) {
+            bottomRightInnie =  (!outOfGrid(down[0], down[1]) && lineArr[down[1]][down[0]] === key) && 
+                                (!outOfGrid(right[0], right[1]) && lineArr[right[1]][right[0]] === key) && 
+                                (lineArr[bottomRight[1]][bottomRight[0]] !== key);
+        }
+
+        cornerCount +=  boolToNumber(topLeftOutie) + 
+                        boolToNumber(topRightOutie) +
+                        boolToNumber(bottomLeftOutie) +
+                        boolToNumber(bottomRightOutie) +
+                        boolToNumber(topLeftInnie) +
+                        boolToNumber(topRightInnie) +
+                        boolToNumber(bottomLeftInnie) +
+                        boolToNumber(bottomRightInnie);
+    });
+
+    return cornerCount;
 };
 
 const getArea : (region: number[][]) => number = (region: number[][]) => {
